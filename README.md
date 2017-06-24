e48470363a09823a24a065f0fca19d31e22e805d
Federico Ghedina <fedeghe@gmail.com>
2017-06-24 18:28:28 +0200

    test updated, fixed a dependency issue, no need to build it

--- README.md
+++ README.md
@@ -5,7 +5,23 @@ This module allows to search a object literal for:
 - **value**
 - **key**:**value**
 
-regexp can be used also, for example the following call are valid:
+
+## install  
+```
+> npm install searchhash
+```
+
+## build it
+```
+> cd node_modules/searchhash  
+
+> ./node_modules/malta/src/bin.js buildtr.json
+// or, if malta is globally installed
+> malta buildtr.json
+```
+
+
+RegExp can be used also, for example the following call are valid:
 
 ```
 var sh = require('searchHash'),
---
+++ test/countries.js
@@ -0,0 +1,27 @@
+var assert = require('assert'),
+	sh = require('../index.js'),
+	
+
+	objs = require('./data/countries.json');
+
+
+describe('Search starts', function() {
+	describe('How many countrynames contains `az`?', function() {
+		var results = sh.forValue(objs, /az/);
+		it('should find 3 elements', function() {
+			assert.equal(3, results.length);
+			for (var i = 0; i < results.length; i++) {
+				console.log('> ' + results[i].value);
+			}
+		});
+	});
+	describe('How many countrynames contains `al`?', function() {
+		var results = sh.forValue(objs, /al/);
+		it('should find 27 elements', function() {
+			assert.equal(27, results.length);
+			for (var i = 0; i < results.length; i++) {
+				console.log('> ' + results[i].value);
+			}
+		});
+	});
+});
