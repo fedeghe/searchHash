070e61df5d88868d1d0ab9b2dc4eaf859d0fea4b
fedeghe <fedeghe@gmail.com>
2019-07-04 22:39:57 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -1,134 +1,14 @@
-
-var searchHash = (function () {
-    var ERRORS = {
-        BAD1: {
-            type: 'BAD_PARAMS',
-            message: 'Either a Literal Object either an array should be passed as second parameter'
-        }
-    };
-
-    // some utility func
-    function jCompare (obj1, obj2) {
-        return JSON.stringify(obj1) === JSON.stringify(obj2);
-    }
-    function isNode (o) {
-        return o &&
-            typeof o === 'object' &&
-            typeof o.nodeType === 'number' &&
-            typeof o.nodeName === 'string';
-    }
-    function isElement (o) {
-        return o &&
-            typeof o === 'object' &&
-            o !== null &&
-            o.nodeType === 1 &&
-            typeof o.nodeName === 'string';
-    }
-    function isString (o) {
-        return typeof o === 'string' || o instanceof String;
-    }
-
-    /**
-     * Main searching function
-     */
-    function digFor (what, obj, target, opts) {
-        var t,
-            startingTime = +new Date(),
-            endingTime = 0,
-            found = 0,
-            matches = {
-                key: function (k1, k2, key) {
-                    return (isString(k1) && key instanceof RegExp)
-                        ? k1.match(key)
-                        : jCompare(k1, key);
-                },
-                value: function (k1, k2, val) {
-                    return (isString(k2) && val instanceof RegExp)
-                        ? k2.match(val)
-                        : jCompare(k2, val);
-                },
-                keyvalue: function (k1, k2, keyval) {
-                    return (
-                        (isString(k1) && keyval.key instanceof RegExp)
-                            ? k1.match(keyval.key)
-                            : jCompare(k1, keyval.key)
-                    ) && (
-                        (isString(k2) && keyval.value instanceof RegExp)
-                            ? k2.match(keyval.value)
-                            : jCompare(k2, keyval.value)
-                    );
-                }
-            }[what],
-            res = {
-                timeElapsed: 0,
-                results: []
-            },
-            maybeDoPush = function (path, index, trg, obj, level) {
-                var p = [].concat.call(path, [index]),
-                    tmp = matches(index, obj[index], trg),
-                    inRange = opts.min <= level && level <= opts.max;
-
-                if (inRange && tmp) {
-                    res.results.push({
-                        obj: obj,
-                        value: obj[index],
-                        key: p[p.length - 1],
-                        parentKey: p[p.length - 2],
-                        path: p.join('/'),
-                        container: p.slice(0, p.length - 1).join('/'),
-                        parentContainer: p.slice(0, p.length - 2).join('/'),
-                        regexp: tmp,
-                        level: level
-                    });
-                    found++;
-                }
-                dig(obj[index], trg, p, level + 1);
-            },
-            dig = function (o, k, path, level) {
-                if (isNode(o) && isElement(o)) throw ERRORS.BAD1;
-                var i, l;
-
-                if (o instanceof Array) {
-                    for (i = 0, l = o.length; i < l; i++) {
-                        maybeDoPush(path, i, k, o, level);
-                        if (opts.limit === found) break;
-                    }
-                } else if (typeof o === 'object') {
-                    for (i in o) {
-                        maybeDoPush(path, i, k, o, level);
-                        if (opts.limit === found) break;
-                    }
-                }
-            };
-
-        opts.limit = 'limit' in opts ? ~~(opts.limit) : Infinity;
-        opts.min = 'min' in opts ? ~~(opts.min) : 0;
-        opts.max = 'max' in opts ? ~~(opts.max) : Infinity;
-
-        opts.min = opts.min < 0 ? 0 : opts.min;
-        if (opts.max < opts.min) {
-            t = opts.min;
-            opts.min = opts.max;
-            opts.max = t;
-        }
-        dig(obj, target, [], 0);
-        endingTime = +new Date();
-        res.timeElapsed = endingTime - startingTime;
-        return res;
-    }
-
-    return {
-        forKey: function (o, k, opts) {
-            return digFor('key', o, k, opts || {});
-        },
-        forValue: function (o, k, opts) {
-            return digFor('value', o, k, opts || {});
-        },
-        forKeyValue: function (o, kv, opts) {
-            return digFor('keyvalue', o, kv, opts || {});
-        }
-    };
-})();
-typeof exports === 'object' &&
-    typeof module !== 'undefined' &&
-    (module.exports = searchHash);
+/*
+SEARCHHASH
+~1KB
+*/
+var searchHash=function(){function e(e,n){return JSON.stringify(e)===JSON.stringify(n)}function n(e){return e&&"object"==typeof e&&"number"==typeof e.nodeType&&"string"==typeof e.nodeName}
+function t(e){return e&&"object"==typeof e&&null!==e&&1===e.nodeType&&"string"==typeof e.nodeName}function i(e){return"string"==typeof e||e instanceof String}function r(r,o,u,f){
+var c,l=+new Date,m=0,s=0,p={key:function(n,t,r){return i(n)&&r instanceof RegExp?n.match(r):e(n,r)},value:function(n,t,r){return i(t)&&r instanceof RegExp?t.match(r):e(t,r)},keyvalue:function(n,t,r){
+return(i(n)&&r.key instanceof RegExp?n.match(r.key):e(n,r.key))&&(i(t)&&r.value instanceof RegExp?t.match(r.value):e(t,r.value))}}[r],y={timeElapsed:0,results:[]},h=function(e,n,t,i,r){
+var a=[].concat.call(e,[n]),o=p(n,i[n],t);f.min<=r&&r<=f.max&&o&&(y.results.push({obj:i,value:i[n],key:a[a.length-1],parentKey:a[a.length-2],path:a.join("/"),container:a.slice(0,a.length-1).join("/"),
+parentContainer:a.slice(0,a.length-2).join("/"),regexp:o,level:r}),s++),g(i[n],t,a,r+1)},g=function(e,i,r,o){if(n(e)&&t(e))throw a.BAD1;var u,c;if(e instanceof Array)for(u=0,
+c=e.length;u<c&&(h(r,u,i,e,o),f.limit!==s);u++);else if("object"==typeof e)for(u in e)if(h(r,u,i,e,o),f.limit===s)break};return f.limit="limit"in f?~~f.limit:1/0,f.min="min"in f?~~f.min:0,
+f.max="max"in f?~~f.max:1/0,f.min=f.min<0?0:f.min,f.max<f.min&&(c=f.min,f.min=f.max,f.max=c),g(o,u,[],0),m=+new Date,y.timeElapsed=m-l,y}var a={BAD1:{type:"BAD_PARAMS",
+message:"Either a Literal Object either an array should be passed as second parameter"}};return{forKey:function(e,n,t){return r("key",e,n,t||{})},forValue:function(e,n,t){return r("value",e,n,t||{})},
+forKeyValue:function(e,n,t){return r("keyvalue",e,n,t||{})}}}();"object"==typeof exports&&"undefined"!=typeof module&&(module.exports=searchHash);
