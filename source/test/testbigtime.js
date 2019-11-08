00d96c5c42091bc687c1a776ef05cbfe90e45a4a
fedeghe <fedeghe@gmail.com>
2019-11-08 08:00:59 +0100

    3984

--- source/test/testbigtime.js
+++ source/test/testbigtime.js
@@ -7,7 +7,7 @@ describe('Search on a big file', () => {
     describe('TIME: How many lots numbered 119?', () => {
         const search = sh.forKeyValue(objs, { key: "LOT_NUM", value: "119" }),
             expectedNum = 89;
-
+        console.log(search.timeElapsed)
         it(`should find the right amount of lots`, () => {
             assert.equal(expectedNum, search.results.length);
         });
