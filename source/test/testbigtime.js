7a587949c54cfc02708db16f2cb347c5c8049827
Federico <federico.ghedina@signavio.com>
2019-11-08 09:47:44 +0100

    compare to func

--- source/test/testbigtime.js
+++ source/test/testbigtime.js
@@ -7,7 +7,15 @@ describe('Search on a big file', () => {
     describe('TIME: How many lots numbered 119?', () => {
         const search = sh.forKeyValue(objs, { key: "LOT_NUM", value: "119" }),
             expectedNum = 89;
-        console.log(search.timeElapsed)
+        console.log('first:', search.timeElapsed)
+        it(`should find the right amount of lots`, () => {
+            assert.equal(expectedNum, search.results.length);
+        });
+    });
+    describe('TIME: How many lots numbered 119?', () => {
+        const search = sh.forKeyValue(objs, { key: "LOT_NUM", value: v => v === "119" }),
+            expectedNum = 89;
+            console.log('second:', search.timeElapsed)
         it(`should find the right amount of lots`, () => {
             assert.equal(expectedNum, search.results.length);
         });
