0eef5486ef47c15da171109d2a2c8dfed0c9b770
fedeghe <fedeghe@gmail.com>
2019-12-09 21:29:39 +0100

    v1.1.4

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,13 @@ SEARCHHASH
 ~1KB
 */
 var searchHash=function(){function e(e,n){return JSON.stringify(e)===JSON.stringify(n)}function n(e){
-return"object"==typeof Node?e instanceof W.Node:e&&"object"==typeof e&&"number"==typeof e.nodeType&&"string"==typeof e.nodeName}function t(e){
-return"object"==typeof HTMLElement?e instanceof W.HTMLElement:e&&"object"==typeof e&&null!==e&&1===e.nodeType&&"string"==typeof e.nodeName}function i(e){return"string"==typeof e||e instanceof String}
-function o(e){return e instanceof RegExp}function r(r,u,a,f){var c,l=+new Date,m=0,y=0,p={key:function(n,t,r){return"function"==typeof r?r(n):i(n)&&o(r)?n.match(r):e(n,r)},value:function(n,t,r){
-return"function"==typeof r?r(t):i(t)&&o(r)?t.match(r):e(t,r)},keyvalue:function(n,t,r){
-return("function"==typeof r.key&&r.key(n)||(i(n)&&o(r.key)?n.match(r.key):e(n,r.key)))&&("function"==typeof r.value&&r.value(t)||(i(t)&&o(r.value)?t.match(r.value):e(t,r.value)))}}[r],s={
-timeElapsed:0,results:[]},v=function(e,n,t,i,o){var r=[].concat.call(e,[n]),u=p(n,i[n],t),a=f.min<=o&&o<=f.max,c=r.length;a&&u&&(s.results.push({obj:i,value:i[n],key:r[c-1],parentKey:r[c-2],
-path:r.join("/"),container:r.slice(0,c-1).join("/"),parentContainer:r.slice(0,c-2).join("/"),regexp:u,level:o}),y++),d(i[n],t,r,o+1)},d=function(e,i,o,r){var u,a
-;if(!n(e)||!t(e))if(e instanceof Array)for(u=0,a=e.length;u<a&&(v(o,u,i,e,r),f.limit!==y);u++);else if("object"==typeof e)for(u in e)if(v(o,u,i,e,r),f.limit===y)break}
-;return f.limit="limit"in f?~~f.limit:1/0,f.min="min"in f?~~f.min:0,f.max="max"in f?~~f.max:1/0,f.min=f.min<0?0:f.min,f.max<f.min&&(c=f.min,f.min=f.max,f.max=c),d(u,a,[],0),m=+new Date,
-s.timeElapsed=m-l,s}return{forKey:function(e,n,t){return r("key",e,n,t||{})},forValue:function(e,n,t){return r("value",e,n,t||{})},forKeyValue:function(e,n,t){return r("keyvalue",e,n,t||{})}}}()
-;"object"==typeof exports&&"undefined"!=typeof module&&(module.exports=searchHash);
+return e&&"object"==typeof e&&null!==e&&(1===e.nodeType||11===e.nodeType)&&"string"==typeof e.nodeName}function t(e){return"string"==typeof e||e instanceof String}function i(e){
+return e instanceof RegExp}function r(r,o,u,f){var c,l=+new Date,m=0,s=0,y={key:function(n,r,a){return"function"==typeof a?a(n):t(n)&&i(a)?n.match(a):e(n,a)},value:function(n,r,a){
+return"function"==typeof a?a(r):t(r)&&i(a)?r.match(a):e(r,a)},keyvalue:function(n,r,a){
+return("function"==typeof a.key&&a.key(n)||(t(n)&&i(a.key)?n.match(a.key):e(n,a.key)))&&("function"==typeof a.value&&a.value(r)||(t(r)&&i(a.value)?r.match(a.value):e(r,a.value)))}}[r],p={
+timeElapsed:0,results:[]},h=function(e,n,t,i,r){var a=[].concat.call(e,[n]),o=y(n,i[n],t),u=f.min<=r&&r<=f.max,c=a.length;u&&o&&(p.results.push({obj:i,value:i[n],key:a[c-1],parentKey:a[c-2],
+path:a.join("/"),container:a.slice(0,c-1).join("/"),parentContainer:a.slice(0,c-2).join("/"),regexp:o,level:r}),s++),v(i[n],t,a,r+1)},v=function(e,t,i,r){if(n(e))throw a.BAD1;var o,u
+;if(e instanceof Array)for(o=0,u=e.length;o<u&&(h(i,o,t,e,r),f.limit!==s);o++);else if("object"==typeof e)for(o in e)if(h(i,o,t,e,r),f.limit===s)break};return f.limit="limit"in f?~~f.limit:1/0,
+f.min="min"in f?~~f.min:0,f.max="max"in f?~~f.max:1/0,f.min=f.min<0?0:f.min,f.max<f.min&&(c=f.min,f.min=f.max,f.max=c),v(o,u,[],0),m=+new Date,p.timeElapsed=m-l,p}var a={BAD1:{type:"BAD_PARAMS",
+message:"Either a Literal Object either an array should be passed as second parameter"}};return{forKey:function(e,n,t){return r("key",e,n,t||{})},forValue:function(e,n,t){return r("value",e,n,t||{})},
+forKeyValue:function(e,n,t){return r("keyvalue",e,n,t||{})}}}();"object"==typeof exports&&"undefined"!=typeof module&&(module.exports=searchHash);
--- source/index.js
+++ source/index.js
@@ -11,28 +11,12 @@ var searchHash = (function () {
     function jCompare (obj1, obj2) {
         return JSON.stringify(obj1) === JSON.stringify(obj2);
     }
-    function isNode (o) {
-        return (
-            typeof Node === 'object'
-                ? o instanceof W.Node
-                : o &&
-                    typeof o === 'object' &&
-                    typeof o.nodeType === 'number' &&
-                    typeof o.nodeName === 'string'
-        );
-    }
-
-    // Returns true if it is a DOM element
-    //
     function isElement (o) {
-        return (
-            typeof HTMLElement === 'object'
-                ? o instanceof W.HTMLElement
-                : o &&
-                    typeof o === 'object' &&
-                    o !== null && o.nodeType === 1 &&
-                    typeof o.nodeName === 'string'
-        );
+        return o &&
+            typeof o === 'object' &&
+            o !== null &&
+            (o.nodeType === 1 || o.nodeType === 11) &&
+            typeof o.nodeName === 'string';
     }
     function isString (o) {
         return typeof o === 'string' || o instanceof String;
@@ -108,18 +92,17 @@ var searchHash = (function () {
                 dig(obj[index], trg, p, level + 1);
             },
             dig = function (o, k, objpath, level) {
+                if (isElement(o)) throw ERRS.BAD1;
                 var i, l;
-                if (!(isNode(o) && isElement(o))) {
-                    if (o instanceof Array) {
-                        for (i = 0, l = o.length; i < l; i++) {
-                            maybePush(objpath, i, k, o, level);
-                            if (opts.limit === found) break;
-                        }
-                    } else if (typeof o === 'object') {
-                        for (i in o) {
-                            maybePush(objpath, i, k, o, level);
-                            if (opts.limit === found) break;
-                        }
+                if (o instanceof Array) {
+                    for (i = 0, l = o.length; i < l; i++) {
+                        maybePush(objpath, i, k, o, level);
+                        if (opts.limit === found) break;
+                    }
+                } else if (typeof o === 'object') {
+                    for (i in o) {
+                        maybePush(objpath, i, k, o, level);
+                        if (opts.limit === found) break;
                     }
                 }
             };
